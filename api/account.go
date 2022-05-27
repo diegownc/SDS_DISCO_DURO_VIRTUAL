dafcb8cb87374d9db5d4defffba4d5fdb448e6e2
diegowncUA <91980297+diegownc@users.noreply.github.com>
2022-05-27 21:20:25 +0200

    Desencriptar con RSA en la parte del servidor

--- api/account.go
+++ api/account.go
@@ -1,27 +1,29 @@
 package api
 
 import (
+	"encoding/base64"
+	"fmt"
 	"net/http"
- 	"fmt"
 	"time"
-	 "github.com/diegownc/SDS_DISCO_DURO_VIRTUAL/token"
-	db "github.com/diegownc/SDS_DISCO_DURO_VIRTUAL/db"
-	"github.com/gin-gonic/gin"
 
+	"io"
 	"log"
 	"os"
-	"io"
+
+	db "github.com/diegownc/SDS_DISCO_DURO_VIRTUAL/db"
+	"github.com/diegownc/SDS_DISCO_DURO_VIRTUAL/token"
+	"github.com/gin-gonic/gin"
 )
 
-const AccessTokenDuration = time.Duration(time.Minute)  
-   
+const AccessTokenDuration = time.Duration(time.Minute)
+
 type loginRequest struct {
 	User     string `form:"username" json:"username" xml:"username" binding:"required"`
 	Password string `form:"password" json:"password" xml:"password" binding:"required"`
 }
 
 type loginResponse struct {
-	Result bool `json:"result"`
+	Result      bool   `json:"result"`
 	AccessToken string `json:"access_token"`
 }
 
@@ -32,7 +34,45 @@ func (server *Server) registrar(ctx *gin.Context) {
 		return
 	}
 
-	res := db.RegistroUsuario(req.User, req.Password)
+	fmt.Println("Usuario Cifrado: ", req.User)
+	fmt.Println()
+	fmt.Println("Contraseña Cifrada: ", req.Password)
+	fmt.Println()
+
+	//Obtenemos nuestra clave privada
+	clavePrivada := leerClavePrivada()
+
+	//Convertimos el cotenido recibido de base64 a bytes[]
+	usernameCifradoBytes, err := base64.Encoding.Strict(*base64.StdEncoding).DecodeString(req.User)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	passwordCifradoBytes, err := base64.Encoding.Strict(*base64.StdEncoding).DecodeString(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	//Desencriptamos con nuestra clave privada...
+	usernameDescifrado, err := RsaDecrypt(usernameCifradoBytes, []byte(clavePrivada))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	passwordDescifrado, err := RsaDecrypt(passwordCifradoBytes, []byte(clavePrivada))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	fmt.Println("Usuario descifrado: ", string(usernameDescifrado))
+	fmt.Println()
+	fmt.Println("Password descifrado: ", string(passwordDescifrado))
+
+	res := db.RegistroUsuario(string(usernameDescifrado), string(passwordDescifrado))
 
 	if res {
 		ctx.JSON(http.StatusOK, "Te has registrado correctamente")
@@ -49,7 +89,45 @@ func (server *Server) login(ctx *gin.Context) {
 		return
 	}
 
-	res := db.LoginUsuario(req.User, req.Password)
+	fmt.Println("Usuario Cifrado: ", req.User)
+	fmt.Println()
+	fmt.Println("Contraseña Cifrada: ", req.Password)
+	fmt.Println()
+
+	//Obtenemos nuestra clave privada
+	clavePrivada := leerClavePrivada()
+
+	//Convertimos el cotenido recibido de base64 a bytes[]
+	usernameCifradoBytes, err := base64.Encoding.Strict(*base64.StdEncoding).DecodeString(req.User)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	passwordCifradoBytes, err := base64.Encoding.Strict(*base64.StdEncoding).DecodeString(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	//Desencriptamos con nuestra clave privada...
+	usernameDescifrado, err := RsaDecrypt(usernameCifradoBytes, []byte(clavePrivada))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	passwordDescifrado, err := RsaDecrypt(passwordCifradoBytes, []byte(clavePrivada))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	fmt.Println("Usuario descifrado: ", string(usernameDescifrado))
+	fmt.Println()
+	fmt.Println("Password descifrado: ", string(passwordDescifrado))
+
+	res := db.LoginUsuario(string(usernameDescifrado), string(passwordDescifrado))
 
 	tokenMaker, err := token.NewJWTMaker("12345678123456781234567812345678")
 
@@ -60,42 +138,42 @@ func (server *Server) login(ctx *gin.Context) {
 
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
- 	} 
+	}
 
-	rsp := loginResponse{ 
-		Result : res,
+	rsp := loginResponse{
+		Result:      res,
 		AccessToken: accessToken,
-	} 
+	}
 	ctx.JSON(http.StatusOK, rsp)
 }
 
 func (server *Server) getUsers(ctx *gin.Context) {
- 
+
 	fmt.Println("METODO QUE REQUIERE AUTENTIFICACION")
 
 }
 
 func (server *Server) uploadFile(ctx *gin.Context) {
-  name := ctx.PostForm("name")
-  fmt.Println(name)
+	name := ctx.PostForm("name")
+	fmt.Println(name)
 
-  file, header, err := ctx.Request.FormFile("upload")
-  if err != nil{
-	  ctx.String(http.StatusBadRequest, "Bad request")
-	  return
-  }
-  filename := header.Filename
+	file, header, err := ctx.Request.FormFile("upload")
+	if err != nil {
+		ctx.String(http.StatusBadRequest, "Bad request")
+		return
+	}
+	filename := header.Filename
 
-  fmt.Println(file, err, filename)
+	fmt.Println(file, err, filename)
 
-  out, err := os.Create(filename)
-  if err != nil{
-	  log.Fatal(err)
-  }
-  defer out.Close()
-  _, err = io.Copy(out, file)
-  if err != nil{
-	  log.Fatal(err)
-  }
-  ctx.JSON(http.StatusCreated, "Upload succesful")
+	out, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+	_, err = io.Copy(out, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx.JSON(http.StatusCreated, "Upload succesful")
 }
---
+++ api/rsa.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"io/ioutil"
+	"log"
+)
+
+// openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
+// convierte el contenido del rsa_private_key.pem en un string
+func leerClavePrivada() string {
+	datosComoBytes, err := ioutil.ReadFile("/home/usuario/Escritorio/SDS_DISCO_DURO_VIRTUAL-main/api/rsa_private_key.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// convertir el arreglo a string
+	datosComoString := string(datosComoBytes)
+	// imprimir el string
+	return datosComoString
+}
+
+// descifrar
+func RsaDecrypt(ciphertext []byte, privateKey []byte) ([]byte, error) {
+	// descifrar
+	block, _ := pem.Decode(privateKey)
+	if block == nil {
+		return nil, errors.New("private key error!")
+	}
+	// Analiza la clave privada en formato PKCS1
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	// descifrar
+	return rsa.DecryptPKCS1v15(rand.Reader, priv, ciphertext)
+}
